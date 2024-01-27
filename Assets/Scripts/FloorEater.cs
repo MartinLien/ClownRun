1b468a52f1da3612cb346dd4b93a094333c997af
wolfcomp <magnus1997.mho@gmail.com>
2024-01-27 14:07:51 +0100

    remove old value not being used

--- Assets/Scripts/FloorEater.cs
+++ Assets/Scripts/FloorEater.cs
@@ -8,8 +8,6 @@ public class FloorEater : MonoBehaviour
     private GroundScrolling _groundScrolling = null;
     [SerializeField] private KillZoneController _killZoneController = null;
 
-    // TODO: This will be a value grabbed from the position helper class that will be used in junction with obstacles to move the mouth closer to the player as they fail to jump over obstacles.
-    private Vector3 _originPosition = Vector3.zero;
     private float _timer = 0;
 
     void Start()
