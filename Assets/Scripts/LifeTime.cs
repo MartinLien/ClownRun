4dd2b52105ba4e1d60c0129f9dc3f153f9bf0a74
wolfcomp <magnus1997.mho@gmail.com>
2024-01-27 15:22:09 +0100

    join some lifetimes together and better object removal

--- Assets/Scripts/LifeTime.cs
+++ Assets/Scripts/LifeTime.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class LifeTime : MonoBehaviour
 {
-    [SerializeField] private float _lifetime = 3f;
+    [SerializeField] private FloatVariable _lifetime = null;
+    [SerializeField] private FloatVariable _fastLifetime = null;
+    [SerializeField] private FloatVariable _groundSpeed = null;
 
     private float _timer = 0;
 
@@ -22,8 +24,13 @@ public class LifeTime : MonoBehaviour
         if (_groundScrolling is { ShouldSpawn: false } && _obstacleSpawner is { ShouldSpawn: false })
             return;
 
-        if (_timer >= _lifetime)
-            Destroy(this.gameObject);
+        if (_timer >= _lifetime.Variable)
+        {
+            if (_timer - _lifetime.Variable >= _fastLifetime.Variable)
+                Destroy(this.gameObject);
+
+            transform.position -= Vector3.forward * _groundSpeed.Variable * 5 * Time.deltaTime;
+        }
 
         _timer += Time.deltaTime;
     }
