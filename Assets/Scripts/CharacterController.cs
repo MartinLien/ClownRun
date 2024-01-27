0ab892cb9d427d2fbd57a84f957d304939fc49d5
wolfcomp <magnus1997.mho@gmail.com>
2024-01-27 16:55:28 +0100

    push some updates around sound files and textures

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -24,6 +24,8 @@ public class CharacterController : MonoBehaviour
     [SerializeField] private AnimationCurve _runningCurve = null;
     [SerializeField] private FloatVariable _runCycleTimer = null;
 
+    [SerializeField] private AudioSource _hitSoundSource = null;
+
     private Lane _prevLane = Lane.Center;
     private bool _running = true;
     private bool _jumping = false;
@@ -82,6 +84,9 @@ public class CharacterController : MonoBehaviour
 
     private void Stunned()
     {
+        _hitSoundSource.pitch = Random.Range(0.8f, 1.2f);
+        _hitSoundSource.time = 0;
+        _hitSoundSource.Play();
         _stunTimer = 0;
         _stunned = true;
     }
